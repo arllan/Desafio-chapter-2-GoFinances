bd1609482adcecc290d00b6feeb82d259c3484b7
Arllan Pablo <arllanpablo@gmail.com>
2022-01-22 20:42:42 -0300

    chore : adicao de funcionalidade de env para proteger dados sensiveis de geracao de build

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,8 @@
-import React, { createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 import * as AuthSession from "expo-auth-session";
+
+const { CLIENT_ID } = process.env; // agora ele vai pegas as variaves descontruindo do arquivo .env
+const { REDIRECT_URI } = process.env; // agora ele vai pegas as variaves descontruindo do arquivo .env
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -17,25 +20,49 @@ interface IAuthContextData {
   signInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
 export const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: "121212",
-    name: "Arllan Pablo",
-    email: "arllanpablo@gmail.com",
-  };
+  // const user = {
+  //   id: "121212",
+  //   name: "Arllan Pablo",
+  //   email: "arllanpablo@gmail.com",
+  // };
+
+  const [user, setUser] = useState<User>({} as User);
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID =
-        "197234747751-hoh30br0477kbppfg60dtc338a7scml9.apps.googleusercontent.com";
-      const REDIRECT_URI = "https://auth.expo.io/@arllan/gofinances";
+      // const CLIENT_ID =
+      //   "197234747751-hoh30br0477kbppfg60dtc338a7scml9.apps.googleusercontent.com";
+      // const REDIRECT_URI = "https://auth.expo.io/@arllan/gofinances";
       const RESPONSE_TYPE = "token";
       const SCOPE = encodeURI("profile email");
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-      const response = await AuthSession.startAsync({ authUrl });
-      console.log(response);
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+        const userInfo = await response.json();
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+        console.log(userInfo);
+      }
     } catch (error: any) {
       throw new Error(error);
     }
